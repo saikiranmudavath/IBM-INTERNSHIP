d162a5d74b218bd517a2ea6378ee88fd3ba012f9
Mudavath Sai Kiran <mudavath.sai.kiran@ibm.com>
2022-01-31 21:58:34 +0530

    first commit

---
+++ Day5/Task1/jip5s/index.js
@@ -0,0 +1,7 @@
+var a = 50;
+var b = 10;
+var add = a + b;
+var mul = a * b;
+var div = a / b;
+var sub = a - b;
+console.log(add, mul, div, sub);
---
+++ Day5/Task3/jip5s/index.js
@@ -0,0 +1,12 @@
+var firstTarget = document.getElementById("app");
+var secondTarget = document.getElementsByTagName("p");
+var thirdTarget = document.getElementsByClassName("para");
+var forthTarget = document.querySelector("para");
+var fifthTarget = document.querySelectorAll("para");
+var childEl = document.getElementById("app");
+console.log(firstTarget);
+console.log(secondTarget);
+console.log(thirdTarget);
+console.log(forthTarget);
+console.log(fifthTarget);
+console.log(childEl.firstChild);
